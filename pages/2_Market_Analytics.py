425a8a7ff6be0dfc8d2fb22622d21a19f0a07d46
Ol1verZZZ <976469957@qq.com>
2022-11-21 15:41:25 -0800

    Feature_Exploration update

--- pages/2_Market_Analytics.py
+++ pages/2_Market_Analytics.py
@@ -44,24 +44,7 @@ with row1_1:
         'Volume': [126, 217, 336, 347, 365, 462, 165]
     })
     if (period == 'Last 3 days'):
-        df = df.iloc[:3]
-    # font = {'family':'Helvetica','weight':'normal','size':13}
-    # fig = plt.figure()
-    # ax2 = fig.add_subplot(111)
-    # ax2.bar(df['Date'], df['Volume'], label='Volume', color='goldenrod')
-    # ax2.legend(prop = {'family':'Helvatica','size':8}, loc='upper left')
-    # ax2.set_ylim([100,500])
-    # ax2.set_ylabel(u'Volume',font)
-    # ax1 = ax2.twinx()
-    # ax1.plot(df['Date'], df['Price'], label='Average Price')
-    # ax1.set_ylim([1,3])
-    # ax1.legend(prop = {'family':'Helvatica','size':8})
-    # ax1.set_ylabel(u'Average Price',font)
-    # plt.xlabel(u'Date',font)
-    # plt.grid(True)
-    # plt.tight_layout()
-    # # plt.show()
-    # st.pyplot(fig)
+        df = df.tail(3)
     fig = make_subplots(specs=[[{"secondary_y": True}]])
     fig.add_trace(
         go.Scatter(
@@ -93,24 +76,7 @@ with row1_2:
         'Ceiling Price': [15.792, 50.000, 32.181, 19.292, 23.101, 28.232, 17.777]
     })
     if (period == 'Last 3 days'):
-        df = df.iloc[:3]
-    # font = {'family':'Helvetica','weight':'normal','size':13}
-    # fig = plt.figure()
-    # ax2 = fig.add_subplot(111)
-    # ax2.plot(df['Date'], df['Floor Price'], label='Floor Price')
-    # ax2.legend(prop = {'family':'Helvatica','size':8}, loc='upper left')
-    # ax2.set_ylim([0.9,1.5])
-    # ax2.set_ylabel(u'Floor Price',font)
-    # ax1 = ax2.twinx()
-    # ax1.plot(df['Date'], df['Ceiling Price'], label='Ceiling Price', color='goldenrod')
-    # ax1.set_ylim([5,50])
-    # ax1.legend(prop = {'family':'Helvatica','size':8})
-    # ax1.set_ylabel(u'Ceiling Price',font)
-    # plt.xlabel(u'Date',font)
-    # plt.grid(True)
-    # plt.tight_layout()
-    # # plt.show()
-    # st.pyplot(fig)
+        df = df.tail(3)
     fig = make_subplots(specs=[[{"secondary_y": True}]])
     fig.add_trace(
         go.Scatter(
@@ -135,14 +101,18 @@ with row1_2:
     fig.update_yaxes(title_text="Floor Price", secondary_y=True, showgrid=False, range=[0,5])
     chart(fig)
 
+st.subheader("Rencent Sales")
 dfTrade=pd.read_csv("recent_sales.csv")
-fig = px.scatter(dfTrade, x="Trade Time", y="Price in USD",hover_name="token_ids",range_x=["2022-11-01","2022-11-09"],range_y=["1800","3000"],
-                title="Recent Sales"
-                )
+if period == 'Last 3 days':
+    date_range = ["2022-11-07","2022-11-09"]
+else:
+    date_range = ["2022-11-03","2022-11-09"]
+fig = px.scatter(dfTrade, x="Trade Time", y="Price in USD",hover_name="token_ids",range_x=date_range,range_y=["1800","3000"])
 chart(fig)
 
+st.subheader("Latest Predicted Price of Otherdeeds in USD")
 prediction_df=pd.read_csv("df_final.csv")
-fig = px.line(prediction_df, x="token_ids", y='price', markers=True,range_x=["500","550"],range_y=[10000,50000],title="Otherdeed Prediction Value in USD",
+fig = px.line(prediction_df, x="token_ids", y='price', markers=True,range_x=["0","100000"],range_y=[0,800000],
              labels={
                      "token_ids": "# of Otherdeed",
                      "price": "USD Dollars"})
